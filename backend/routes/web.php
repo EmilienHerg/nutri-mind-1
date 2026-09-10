0cbda7057dd9d0f07e84c143e64a98afc1bb95f0
Emilien HERGOTT <emilien.hergott@mewo-campus.fr>
2026-09-10 08:45:04 +0200

    CRUD user + gestion des messages d'erreurs

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -9,6 +9,9 @@ Route::get('/', function () {
 });
 
 Route::get('/users', [UserController::class, 'getAllUsers']);
+Route::get('/user/{id}', [UserController::class, 'getUserById']);
+Route::post('/users', [UserController::class, 'addUser']);
+Route::put('/user/{id}', [UserController::class, 'editUser']);
+Route::delete('/user/{id}', [UserController::class, 'deleteUser']);
 
 Route::get('/recipes', [RecipeController::class, 'getAllRecipes']);
-Route::post('');
